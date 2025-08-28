659074e6b742ac3409e7603dfa9fe29f0e607271
okekedev <okekedev@outlook.com>
2025-08-27 23:08:01 -0500

    Initial Commit

---
+++ Narration/NarrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NarrationApp.swift
+//  Narration
+//
+//  Created by Christian  on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct NarrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NarrationTests/NarrationTests.swift
@@ -0,0 +1,17 @@
+//
+//  NarrationTests.swift
+//  NarrationTests
+//
+//  Created by Christian  on 8/27/25.
+//
+
+import Testing
+@testable import Narration
+
+struct NarrationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
